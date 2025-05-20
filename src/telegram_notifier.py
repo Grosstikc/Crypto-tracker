182a55614c320dd6db96534829d99f74fd245d27
Rostyslav Shypita <rolikc19@gmail.com>
2025-05-20 13:07:36 +0200

    Updating telegram_notifier.py

--- src/telegram_notifier.py
+++ src/telegram_notifier.py
@@ -1,12 +1,20 @@
 import os
+import aiohttp
 from dotenv import load_dotenv
-from telegram import Bot
 
 load_dotenv()
 
 TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-async def send_telegram_message(message):
-    bot = Bot(token=TELEGRAM_API_TOKEN)
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+async def send_telegram_message(message, chat_id):
+    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload) as resp:
+            if resp.status != 200:
+                print(f"Failed to send message: {await resp.text()}")
